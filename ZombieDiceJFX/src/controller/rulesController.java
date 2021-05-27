acd315452646ee90b85d6f881317522ea25949f3
Grizfreak <ninotnaaubert@gmail.com>
2021-05-27 09:39:43 +0200

    sa

--- ZombieDiceJFX/src/controller/rulesController.java
+++ ZombieDiceJFX/src/controller/rulesController.java
@@ -4,7 +4,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import application.Main;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.stage.Stage;
